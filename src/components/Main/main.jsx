65e1c9183caa9f70cc3ad3c696973b7d865a22db
LeandroAcipreste <logacipreste@gmail.comLeasou@22676clear>
2024-10-24 19:49:37 -0300

    correçoes de responsividade

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -6,10 +6,14 @@ export default function Main() {
     return(
         <div className='container mt-5 d-flex justify-content-center align-items-center'>
             <div className="row">
-                <div className="col-6">
+                <div className="col-sw-12 col-md-6">
                     <img src='./src/assets/img-projeto.JPG' alt="imagem da página de login" />
                 </div>
-                <div className="col-6"><Formulario/></div>
+                <div className="col-sm-12 text-center text-md-start col-md-6">
+                    <h5 className='col-sw'>Bem-vindo de volta</h5>
+                    <h2 className='col-sw'>Faça login na sua conta</h2>
+                    <Formulario/>
+                    </div>
             </div>
         </div>
     )
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import Main from "../components/Main/main";
+
+
+
+
+export default function Home(){
+    return(
+        <>  
+            <Main/>
+        </>
+    )
+}
