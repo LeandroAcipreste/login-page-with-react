51ade65098a713439cc03c577cf1fcc8c2d1018b
LeandroAcipreste <logacipreste@gmail.comLeasou@22676clear>
2024-11-20 10:56:02 -0300

    ajustando a responsividade

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,7 +7,7 @@ export default function Header(){
             <div className="container">
                 <div className="row">
                     <div className="col-12 my-5 header-box-img d-flex justify-content-center align-items-center w-100">
-                        <img src="src\assets\content.png" alt="jogo da noite lakers contra chicago bulls" className='' />
+                        <img src="src\assets\content.png" alt="jogo da noite lakers contra chicago bulls" className="w-50" />
                     </div>
                 </div>
             </div>
--- src/components/section/section_main.jsx
+++ src/components/section/section_main.jsx
@@ -7,7 +7,7 @@ export default function Section_main(){
         <section className='section-main'>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-6 section-main-box d-flex justify-content-center mb-5">
+                    <div className="col-sm-12 col-md-6 section-main-box d-flex justify-content-center mb-5 carousel-size-sm">
                         <img src="src\assets\main (2).png" alt="" className='w-100 section-main-box-img' />
                     </div>
                 </div>
@@ -16,7 +16,7 @@ export default function Section_main(){
                     
                     <div class="carousel-item active">
                     <div class="row">
-                        <div class="col-12 col-md-6">
+                        <div class="col-sm-12 col-md-6">
                         <div class="card">
                             <img src="src\assets\Lebron.png" className="card-img" alt="LeBron James"/>
                             <div class="card-img-overlay text-white d-flex align-items-end">
@@ -24,7 +24,7 @@ export default function Section_main(){
                             </div>
                         </div>
                         </div>
-                        <div class="col-12 col-md-6">
+                        <div class="col-sm-12 col-md-6">
                         <div class="card">
                             <img src="src\assets\kobe.png" className="card-img" alt="Kobe Bryant"/>
                             <div class="card-img-overlay text-white d-flex align-items-end">
@@ -36,7 +36,7 @@ export default function Section_main(){
                     </div>
                     <div class="carousel-item">
                     <div class="row">
-                        <div class="col-12 col-md-6">
+                        <div class="col-sm-12 sm-mt-2 col-md-6">
                         <div class="card">
                             <img src="src\assets\Cavaliers.jpg" className="card-img" alt="Imagem 3"/>
                             <div class="card-img-overlay text-white d-flex align-items-end">
@@ -44,7 +44,7 @@ export default function Section_main(){
                             </div>
                         </div>
                         </div>
-                        <div class="col-12 col-md-6">
+                        <div class="col-sm-12 sm-mt-2 col-md-6">
                         <div class="card">
                             <img src="src\assets\jason.jpg" className="card-img" alt="Imagem 4"/>
                             <div class="card-img-overlay text-white d-flex align-items-end">
